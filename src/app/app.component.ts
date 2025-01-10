4a52ca4ac1415a2c5921fc24942e360ced320bdd
Ahmed Emara <ahmedhossamemara8@gmail.com>
2025-01-10 18:31:00 +0200

    added footer component and removed contact component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { HeroComponent } from './hero/hero.component';
 import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
-import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +17,7 @@ import { ContactComponent } from './contact/contact.component';
     AboutComponent,
     SkillsComponent,
     ProjectsComponent,
-    ContactComponent,
+    FooterComponent,
   ]
 })
 export class AppComponent {
