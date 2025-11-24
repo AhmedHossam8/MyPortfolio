911796bab4c384817b6400e95e83d2c14c4943da
AhmedHossam8 <ahmedhossamemara8@gmail.com>
2025-11-24 10:57:24 +0200

    fixed routes error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,9 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: null },
-    { path: 'about', component: null },
-    { path: 'skills', component: null },
-    { path: 'projects', component: null },
+    { path: 'home', children: [] },
+    { path: 'about', children: [] },
+    { path: 'skills', children: [] },
+    { path: 'projects', children: [] },
     { path: '**', redirectTo: 'home' }
 ];
