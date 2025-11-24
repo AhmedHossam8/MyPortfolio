31bf05d11cbad30f0a121d80c5dbaefe5804eecc
AhmedHossam8 <ahmedhossamemara8@gmail.com>
2025-11-24 10:48:08 +0200

    fixed the app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: null },
+    { path: 'about', component: null },
+    { path: 'skills', component: null },
+    { path: 'projects', component: null },
+    { path: '**', redirectTo: 'home' }
+];
